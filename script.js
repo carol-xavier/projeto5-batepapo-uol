69a4aef10a4e46cebf05b97077ca6cb037dcc9dc
CarolXavier <xavier.biotec@gmail.com>
2022-02-10 14:41:01 -0300

    Versão#4 - Cadastro de usuário no sevidor / A página atualiza a cada 3s

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// function cadastrarNome(){
-//    nomeUsuario = prompt("Por qual nome você gosta de ser chamado?");
-// }
-
 function abrirMenu() {
   const menubar = document.querySelectorAll('.escondido');
   for (i = 0; i < menubar.length; i++){
@@ -21,6 +17,39 @@ function fecharMenu(){
 
 /*--------------------Requisitos-------------------------*/
 let chat = [];
+let name = "";
+cadastrarUsuario();
+buscarMensagens();
+window.setInterval(buscarMensagens,3000);
+
+function cadastrarUsuario() {
+  name = prompt("Como você gosta de ser chamado?");
+
+  let objeto = {name:name};
+  const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", objeto);
+
+  promise.then(cadastrarSucesso);
+  promise.catch(deuErro);
+}
+
+function deuErro(erro) {
+  console.log(erro.response);
+  let statusCode = parseInt(erro.response.status);
+  if(statusCode === 400){
+    name = prompt("Nome de usuário já existente. Tente com um nome diferente.");
+    
+    let objeto = {name:name};
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", objeto);
+
+    promise.then(cadastrarSucesso);
+    promise.catch(deuErro);
+  }
+}
+
+function cadastrarSucesso() {
+  alert("Cadastro realizado com sucesso!");
+}
+
 
 function buscarMensagens() {
   const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
@@ -32,56 +61,56 @@ function mostrarMensagem(resposta) {
   renderizarMensagem();
 }
 
+
 function renderizarMensagem() {
+  const data = new Date();
+  const time =`${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`;
+
   const mensagem = document.querySelector("article");
   mensagem.innerHTML = "";
 
-for (let i = 0; i < chat.length; i++){    
-if(chat[i].type == "status"){
-  mensagem.innerHTML += `
-        <div class="notificationStatus">
-            <div class="time">(${chat[i].time})</div>
-            <div class="msgContent"><strong>${chat[i].from}</strong> ${chat[i].text}</div>
-        </div>`
- }
- if(chat[i].type == "message"){
-   mensagem.innerHTML +=   `    
-         <div class="notificationMsgNormal">
-            <div class="time">(${chat[i].time})</div>
-            <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
-        </div>
-   `}
-  if(chat[i].to !== "todos"){`
-        <div class="notificationMsgReservada">
-             <div class="time">(${chat[i].time})</div>
-             <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
-         </div>
-        `}
-   }
- }
-
- function adicionarMensagem() {
-  const msgContent = document.querySelector(".msgChat").value;
-  const data = new Date();
-  const hora =`${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`;
-  const from = "Carol";
-  const to = "Todos";
-  const type = "message";
-
-     if(msgContent && hora && from && to && type){
-     const novaMensagem = {
-       text: msgContent, 
-       time: hora,                    
-       from:from, 
-       to:to, 
-       type:type};
-     chat.push(novaMensagem);
-     }
-    
-  // const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages");
-  // console.log(chat);             
-  // promise.then(renderizarMensagem);   //Bad request error 400!!!!!!!!!!!!!!!!!
+    for (let i = 0; i < chat.length; i++){    
+    if(chat[i].type == "message"){ 
+      mensagem.innerHTML +=   `    
+            <div  class="notificationMensagem">
+                <div class="time">(${chat[i].time})</div>
+                <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
+            </div>
+      `} else{
+        mensagem.innerHTML += `
+              <div  class="notificationStatus">
+                  <div class="time">(${chat[i].time})</div>
+                  <div class="msgContent"><strong>${chat[i].from}</strong> ${chat[i].text}</div>
+              </div>`
+      }                              
+      if(chat[i].to == name){`  
+            <div  class="notificationMsgReservada">
+                <div class="time">(${chat[i].time})</div>
+                <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
+            </div>
+            `} //perguntar se esse if de Reservada está correto
+      // scrollAutomatico();
+      }
 }
 
-buscarMensagens();
-window.setInterval(buscarMensagens,3000);
+// function scrollAutomatico(){
+//       //rever isso aqui do scrollIntoView, achar o querySelector correto
+//       const elementoNovoBottom = document.querySelector(".novaMensagem");
+//       elementoNovoBottom.scrollIntoView()
+// }
+
+
+ function adicionarMensagem() {
+
+     const objeto = {
+       text: document.querySelector(".msgChat").value,                  
+       from: name, 
+       to: "Todos", 
+       type: "message"};
+
+       console.log(objeto);
+
+  const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", objeto);           
+  promise.then(buscarMensagens);  
+  }
+    
