c83cbda5256a45bcfc02b72930e75cb1a42b67d0
CarolXavier <xavier.biotec@gmail.com>
2022-02-14 16:18:56 -0300

    Versão#9 - Bônus: página inicial adicionada

--- script.js
+++ script.js
@@ -13,10 +13,8 @@ function fecharMenu() {
 }
 
 
-let input = document.querySelector(".msgChat");
-
-
-input.addEventListener("keyup", function (KeyboardEvent) {
+let inputMensagem = document.querySelector(".msgChat");
+inputMensagem.addEventListener("keyup", function (KeyboardEvent) {
   // Number 13 is the "Enter" key on the keyboard
   if (KeyboardEvent.keyCode === 13) {
     event.preventDefault();
@@ -25,37 +23,47 @@ input.addEventListener("keyup", function (KeyboardEvent) {
 });
 
 
+let botaoEntrar = document.querySelector(".nomeUsuario");
+botaoEntrar.addEventListener("keyup", function (KeyboardEvent) {
+  if (KeyboardEvent.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".botaoEntrar").click();
+  }
+});
+
 /*--------------------Requisitos-------------------------*/
 let chat = [];
 let name = "";
 const elementoNovoBottom = [];
 
 function cadastrarUsuario() {
-  name = prompt("Como você gosta de ser chamado?");
+  name = document.querySelector(".nomeUsuario").value;
 
   let objeto = { name: name };
   const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", objeto);
 
   promise.then(cadastrarSucesso);
-  promise.catch(deuErro);
+  promise.catch(nomeJaCadastrado);
 }
 
-function deuErro(erro) {
+function nomeJaCadastrado(erro) {
+  let deuErro = alert("Nome de usuário já existente. Tente com um nome diferente.");
   console.log(erro.response);
+
   let statusCode = parseInt(erro.response.status);
   if (statusCode === 400) {
-    name = prompt("Nome de usuário já existente. Tente com um nome diferente.");
-
-    let objeto = { name: name };
-    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", objeto);
-
-    promise.then(cadastrarSucesso);
-    promise.catch(deuErro);
+    setTimeout(nomeJaCadastrado(),5000);
   }
 }
 
 function cadastrarSucesso() {
   alert("Cadastro realizado com sucesso!");
+
+  const usuarioCadastrado = document.querySelector(".tela-inicial");
+  usuarioCadastrado.classList.add("escondido");
+
+  iniciarChecarStatus()
+  iniciarBuscarMensagens()
 }
 
 function buscarMensagens() {
@@ -72,8 +80,6 @@ function mostrarMensagem(resposta) {
 function scrollAutomatico() {
   const todasMensagens = document.querySelectorAll("article div")
   const ultimaMensagem = todasMensagens[todasMensagens.length - 1];
-  console.log("ultimaMensagem", ultimaMensagem);
-
   ultimaMensagem.scrollIntoView();
 }
 
@@ -87,20 +93,21 @@ function renderizarMensagem() {
   for (let i = 0; i < chat.length; i++) {
     if (chat[i].type == "message") {
       mensagem.innerHTML += `    
-            <div  class="notificationMensagem data-identifier="message"">
+            <div  class="notificationMensagem messagebox data-identifier="message"">
                 <div class="time">(${chat[i].time})</div>
                 <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
             </div>`
-    } else{
+    } 
+    if(chat[i].type == "status"){
       mensagem.innerHTML += `
-              <div  class="notificationStatus data-identifier="message"">
+              <div  class="notificationStatus messagebox data-identifier="message"">
                   <div class="time">(${chat[i].time})</div>
                   <div class="msgContent"><strong>${chat[i].from}</strong> ${chat[i].text}</div>
               </div>`
     }
     if (chat[i].type == "private_message" && (chat[i].to == name || chat[i].from == name)){
       mensagem.innerHTML += `  
-            <div  class="notificationMsgReservada data-identifier="message"">
+            <div  class="notificationMsgReservada messagebox data-identifier="message"">
                 <div class="time">(${chat[i].time})</div>
                 <div class="msgContent"><strong>${chat[i].from}</strong> para <strong>${chat[i].to}</strong>: ${chat[i].text}</div>
             </div>
@@ -112,7 +119,6 @@ function renderizarMensagem() {
 
 
 function adicionarMensagem() {
-
   const objeto = {
     text: document.querySelector(".msgChat").value,
     from: name,
@@ -121,22 +127,25 @@ function adicionarMensagem() {
   };
 
   console.log(objeto);
-
   const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", objeto);
   promise.then(buscarMensagens);
   promise.catch(barrarUsuarioOff);
 }
+
 
 function barrarUsuarioOff() {
   window.location.reload();
 }
 
 
-cadastrarUsuario();
-buscarMensagens();
-window.setInterval(buscarMensagens, 3000);
-window.setInterval(function () {           //usando função anônima
-  let objeto = { name: name };
-  const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", objeto);
-}, 5000);
+function iniciarChecarStatus() {
+  window.setInterval(function () {           //usando função anônima
+    let objeto = { name: name };
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", objeto);
+  }, 4000);
+}
+
+function iniciarBuscarMensagens() {
+  window.setInterval(buscarMensagens, 3000);
+}
 
